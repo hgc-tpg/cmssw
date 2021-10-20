32448ce6fba278a19a32c09ca73303e076c208f8
Becheva <becheva@llr.in2p3.fr>
2021-10-20 16:48:00 +0200

    test 5 trigger the PR for particular branch name

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jen_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jen_cfg.py
@@ -106,7 +106,7 @@ process.hgcalValidationTPG_step = cms.Path(process.runHGCALValidationTPG)
 #process.dqmSaver.workflow = '/validation/' + 'HGCAL' + '/TPG'
 #process.dqmsave_step = cms.Path(process.dqmSaver)
 
-# NEW added test4
+# NEW added test5
 process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
