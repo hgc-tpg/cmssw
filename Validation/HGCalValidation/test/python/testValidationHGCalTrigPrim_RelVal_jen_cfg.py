a4cbd8b8dee1492f206e1b14dbc1932c0145fd65
ebecheva <emilia.becheva@llr.in2p3.fr>
2021-12-08 15:07:36 +0100

    Update testValidationHGCalTrigPrim_RelVal_jen_cfg.py
    Test how Jenkins manage PR

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jen_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_jen_cfg.py
@@ -106,7 +106,7 @@ process.hgcalValidationTPG_step = cms.Path(process.runHGCALValidationTPG)
 #process.dqmSaver.workflow = '/validation/' + 'HGCAL' + '/TPG'
 #process.dqmsave_step = cms.Path(process.dqmSaver)
 
-# NEW added test13
+# NEW added test14
 process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
