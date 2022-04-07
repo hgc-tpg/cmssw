674e03b1de83d5243a1ccefb63bc85e16320c54e
ebecheva <emilia.becheva@llr.in2p3.fr>
2022-04-07 15:10:22 +0200

    testing Status checks in Jenkins
    testing Status checks in Jenkins

--- Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_cfg.py
+++ Validation/HGCalValidation/test/python/testValidationHGCalTrigPrim_RelVal_cfg.py
@@ -106,7 +106,7 @@ process.hgcalValidationTPG_step = cms.Path(process.runHGCALValidationTPG)
 #process.dqmSaver.workflow = '/validation/' + 'HGCAL' + '/TPG'
 #process.dqmsave_step = cms.Path(process.dqmSaver)
 
-# NEW added test removing Jenkins messeges in GitHub
+# NEW added testing Status checks in Jenkins
 process.onlineSaver = cms.EDAnalyzer("DQMFileSaverOnline",
     producer = cms.untracked.string('DQM'),
     path = cms.untracked.string('./'),
