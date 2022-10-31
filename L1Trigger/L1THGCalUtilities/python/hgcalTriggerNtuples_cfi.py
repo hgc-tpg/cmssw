f2df70498484102b51fe31971487590c558357bc
Emyr Clement <emyr.clement@bristol.ac.uk>
2022-10-31 09:12:49 +0000

    Correct ntuple settings.

--- L1Trigger/L1THGCalUtilities/python/hgcalTriggerNtuples_cfi.py
+++ L1Trigger/L1THGCalUtilities/python/hgcalTriggerNtuples_cfi.py
@@ -92,7 +92,7 @@ ntuple_multiclusters = cms.PSet(
     NtupleName = cms.string('HGCalTriggerNtupleHGCMulticlusters'),
     Multiclusters = cms.InputTag('hgcalBackEndLayer2Producer:HGCalBackendLayer2Processor3DClustering'),
     EGIdentification = egamma_identification_histomax.clone(),
-    FillLayerInfo = cms.bool(True),
+    FillLayerInfo = cms.bool(False),
     FillInterpretationInfo = cms.bool(True),
     FillHWClusterProperties = cms.bool(False)
 )
