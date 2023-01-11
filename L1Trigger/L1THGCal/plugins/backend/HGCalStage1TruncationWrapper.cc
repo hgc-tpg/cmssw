d062e745f399dc0bd49d9756226255bd222a8bd6
Louis Portales <louis.portales@gmail.com>
2023-01-11 10:26:52 +0100

    removing useless comment

--- L1Trigger/L1THGCal/plugins/backend/HGCalStage1TruncationWrapper.cc
+++ L1Trigger/L1THGCal/plugins/backend/HGCalStage1TruncationWrapper.cc
@@ -64,7 +64,7 @@ void HGCalStage1TruncationWrapper::convertCMSSWInputs(const std::vector<edm::Ptr
         rozBin(roverz,
                theConfiguration_.rozMin(),
                theConfiguration_.rozMax(),
-               theConfiguration_.rozBins());  //(std::sqrt(x * x + y * y) / std::abs(z)) * FWfactor_;
+               theConfiguration_.rozBins());
     double phi = rotatedphi(tc->phi(), theConfiguration_.phiSector());
     phi += (phi < 0) ? M_PI : 0;
     unsigned int digi_phi = phi * FWfactor_;
